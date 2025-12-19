8d14c31048e73ca29d9582c123a52e60d506cf66
visiblecho <121157554+visiblecho@users.noreply.github.com>
2025-12-19 15:58:38 +0100

    Update main.jsx to include basename of repo

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import { UserProvider } from './contexts/UserContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/purchaseiq">
       <ThemeProvider>
         <UserProvider>
           <App />
